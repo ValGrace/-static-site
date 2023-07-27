7f1fa39f1d0e7788a8a588aa65b50be021024076
Val Grace <gracevalerie1@gmail.com>
2023-07-27 10:06:39 +0300

    chore: add nixpacks file

---
+++ .history/client/src/Routing/PageRouter_20230727065125.jsx
@@ -0,0 +1,19 @@
+import Dashboard from "../Pages/Dashboard";
+import HomePage from "../Pages/HomePage";
+import { Routes, Route} from "react-router-dom"
+import AboutPage from "../Pages/AboutPage";
+import Articles from "../Pages/Articles";
+import Contents from "../Pages/Contents";
+const PageRouter = () => {
+    return (
+        <Routes>
+            <Route exact path="/" element={ <HomePage />}/>
+            <Route path="/login" element={ <Dashboard />}/>
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<Articles />} />
+            <Route exact path="/article/:projectID" element={<Contents />} />
+        </Routes>
+    )
+}
+
+export default PageRouter
